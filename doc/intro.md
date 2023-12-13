af77bbe8b7400cd4e3511a029c450b0cfb1dc146
roboli <robertooliveros@mac.com>
2023-12-13 05:58:26 -0600

    Import lein files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tennis
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tennis/core_test.clj
@@ -0,0 +1,7 @@
+(ns tennis.core-test
+  (:require [clojure.test :refer :all]
+            [tennis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
