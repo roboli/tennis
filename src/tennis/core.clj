d96fdb9958858d299aef7efb165ede498d042513
roboli <robertooliveros@mac.com>
2023-12-15 05:42:53 -0600

    Get post example

--- src/tennis/core.clj
+++ src/tennis/core.clj
@@ -6,3 +6,12 @@
   [host handle]
   (let [agent (api/service host)]
     (agent :resolve-handle handle)))
+
+(defn get-post-thread
+  "Test getting post thread"
+  [host uri]
+  (let [response (api/get-post-thread host uri)
+        post     (get-in response [:data :thread :post])]
+    {:author (get-in post [:author :handle])
+     :name   (get-in post [:author :displayName])
+     :text   (get-in post [:record :text])}))
