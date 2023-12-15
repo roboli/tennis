fbe0708e87704361302b2f8b0c8d849fe1e47340
roboli <robertooliveros@mac.com>
2023-12-15 05:38:09 -0600

    Parse body to edn and return with headers

--- src/tennis/xrpc.clj
+++ src/tennis/xrpc.clj
@@ -1,5 +1,6 @@
 (ns tennis.xrpc
   (:require [clj-http.client :as client]
+            [cheshire.core :refer [parse-string]]
             [tennis.lexicon :as lex]))
 
 (defn process-request
@@ -7,8 +8,10 @@
   ([host nsid params body]
    (if-let [schema (get lex/schemas nsid)]
      (let [type (get-in schema [:defs :main :type])
-           url  (str "https://" host "/xrpc/" nsid)]
-       (if (= "query" type)
-         (client/get url {:query-params params})
-         (client/post url {:query-params params}
-                      body))))))
+           url  (str "https://" host "/xrpc/" nsid)
+           res  (if (= "query" type)
+                  (client/get url {:query-params params})
+                  (client/post url {:query-params params}
+                               body))]
+       {:data (parse-string (:body res) true)
+        :headers (:headers res)}))))
