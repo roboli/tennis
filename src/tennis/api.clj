8befc2089cc2c42a886bf020d7b85aa43a08ed35
roboli <robertooliveros@mac.com>
2023-12-15 05:42:19 -0600

    Split into methods for optional calling directly

--- src/tennis/api.clj
+++ src/tennis/api.clj
@@ -1,7 +1,14 @@
 (ns tennis.api
   (:require [tennis.xrpc :as xrpc]))
 
+(defn get-post-thread [host uri]
+  (xrpc/process-request host "app.bsky.feed.getPostThread" {"uri" uri}))
+
+(defn resolve-handle [host handle]
+  (xrpc/process-request host "com.atproto.identity.resolveHandle" {"handle" handle}))
+
 (defn service [host]
   (fn [method & args]
-    (if (= method :resolve-handle)
-      (xrpc/process-request host "com.atproto.identity.resolveHandle" {"handle" (first args)}))))
+    (condp = method
+      :get-post-thread (get-post-thread host (first args))
+      :resolve-handle  (resolve-handle host (first args)))))
